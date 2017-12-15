e1b850eff269b65aa4f1c43b01efa716b5632df1
Leessang <992275294@qq.com>
2017-12-15 16:15:08 +0800

    创建测滑过度类

---
+++ KNSlippageMenu/KNSilppageController/KNTransitionMethod.h
@@ -0,0 +1,16 @@
+//
+//  KNTransitionMethod.h
+//  KNSlippageMenu
+//
+//  Created by 刘凡 on 2017/12/15.
+//  Copyright © 2017年 leesang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "KNSlippageConfig.h"
+
+
+@interface KNTransitionMethod : NSObject
+
+@end
---
+++ KNSlippageMenu/KNSilppageController/KNTransitionMethod.m
@@ -0,0 +1,13 @@
+//
+//  KNTransitionMethod.m
+//  KNSlippageMenu
+//
+//  Created by 刘凡 on 2017/12/15.
+//  Copyright © 2017年 leesang. All rights reserved.
+//
+
+#import "KNTransitionMethod.h"
+
+@implementation KNTransitionMethod
+
+@end
