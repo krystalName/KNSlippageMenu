e299dc4410e2b5e4a75dff4fc33d9c7662a036de
Leessang <992275294@qq.com>
2017-12-15 17:47:55 +0800

    完成过度类.h

--- KNSlippageMenu/KNSilppageController/KNTransitionMethod.h
+++ KNSlippageMenu/KNSilppageController/KNTransitionMethod.h
@@ -11,6 +11,58 @@
 #import "KNSlippageConfig.h"
 
 
+//状态
+typedef NS_ENUM(NSUInteger, KNTransitionMethodType){
+    KNTransitionMethodTypeShow = 0,
+    KNTransitionMethodTypeHideed
+};
+
+///动画类型
+typedef NS_ENUM(NSInteger , KNTransitionAnimation){
+    KNTransitionAnimationDefault = 0,
+    KNTransitionAnimationMask
+};
+
+
+
 @interface KNTransitionMethod : NSObject
 
+
+/**
+ 实例方法创建
+
+ @param methodType 状态。 是隐藏还是现实
+ @param animationType 动画。默认的还是mask
+ @param SlippageConfig 滑动的基本配置
+ @return 返回本身
+ */
+-(instancetype)initWithMethodType:(KNTransitionMethodType)methodType AnimationType:(KNTransitionAnimation)animationType SlippageConfig:(KNSlippageConfig *)SlippageConfig;
+
+/**
+ 类方法创建
+ 
+ @param methodType 状态。 是隐藏还是现实
+ @param animationType 动画。默认的还是mask
+ @param SlippageConfig 滑动的基本配置
+ @return 返回本身
+ */
++(instancetype)initWithMethodType:(KNTransitionMethodType)methodType AnimationType:(KNTransitionAnimation)animationType SlippageConfig:(KNSlippageConfig *)SlippageConfig;
+
 @end
+
+@interface MaskView :UIView <UIGestureRecognizerDelegate>
+
+@property (nonatomic, copy)NSArray *toViewSubViews;
+
+/**
+ 创建单例
+ 
+ @return 返回本身
+ */
++(instancetype)shareInstance;
+
+///释放单例
++(void)releaseInstance;
+
+@end
+
