1a09232bf99468b98675c84dc05ddedded5116fe
Leessang <992275294@qq.com>
2017-12-19 14:45:25 +0800

    创建横向滑动动画

---
+++ KNSlippageMenu/KNSilppageController/KNLateralSlideAnimat.h
@@ -0,0 +1,13 @@
+//
+//  KNLateralSlideAnimat.h
+//  KNSlippageMenu
+//
+//  Created by 刘凡 on 2017/12/19.
+//  Copyright © 2017年 leesang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KNLateralSlideAnimat : NSObject
+
+@end
---
+++ KNSlippageMenu/KNSilppageController/KNLateralSlideAnimat.m
@@ -0,0 +1,13 @@
+//
+//  KNLateralSlideAnimat.m
+//  KNSlippageMenu
+//
+//  Created by 刘凡 on 2017/12/19.
+//  Copyright © 2017年 leesang. All rights reserved.
+//
+
+#import "KNLateralSlideAnimat.h"
+
+@implementation KNLateralSlideAnimat
+
+@end
