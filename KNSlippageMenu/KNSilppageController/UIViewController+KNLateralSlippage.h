522e2332204449638ad01459863c8a7d07e543e2
Leessang <992275294@qq.com>
2017-12-19 16:37:06 +0800

    创建UIViewController的分类

---
+++ KNSlippageMenu/KNSilppageController/UIViewController+KNLateralSlippage.h
@@ -0,0 +1,15 @@
+//
+//  UIViewController+KNLateralSlippage.h
+//  KNSlippageMenu
+//
+//  Created by 刘凡 on 2017/12/19.
+//  Copyright © 2017年 leesang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "KNLateralSlideAnimat.h"
+#import "KNSlippageConfig.h"
+
+@interface UIViewController (KNLateralSlippage)
+
+@end
---
+++ KNSlippageMenu/KNSilppageController/UIViewController+KNLateralSlippage.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+KNLateralSlippage.m
+//  KNSlippageMenu
+//
+//  Created by 刘凡 on 2017/12/19.
+//  Copyright © 2017年 leesang. All rights reserved.
+//
+
+#import "UIViewController+KNLateralSlippage.h"
+
+@implementation UIViewController (KNLateralSlippage)
+
+@end
